04ea630913c896757265e967619f5b2397b43e25
Jacob Peng <jacob.peng@hotmail.com>
2017-08-29 12:41:15 -0400

    Added basic routing with dummy implementations of pages

--- src/App.js
+++ src/App.js
@@ -1,68 +1,111 @@
 import React, { Component } from 'react';
 import { PageHeader, Grid, Row, Col, Nav, Navbar, NavItem } from 'react-bootstrap';
 import './App.css';
+
 // Icons
 import Youtube from 'react-icons/lib/fa/youtube-square';
 import Github from 'react-icons/lib/fa/github';
 import Facebook from 'react-icons/lib/fa/facebook-square';
 
+// Routing.
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
+
 function HomeHeader() {
   return (
-    <PageHeader className="text-center">
-      <div className="tagline-upper">Jacob Mai</div>
-      <small className="tagline-lower">
+    <PageHeader className='text-center'>
+      <div className='tagline-upper'>Jacob Mai</div>
+      <small className='tagline-lower'>
         Musician, Developer, Tester
       </small>
     </PageHeader>
   );
 }
 
-function HomeNavigation() {
-  return(
-    <Navbar fluid collapseOnSelect>
-      <Navbar.Toggle />
-      <Navbar.Collapse>
-        <Nav pullRight>
-          <NavItem eventKey={1} href='#'>Music</NavItem>
-          <NavItem eventKey={2} href='#'>Code</NavItem>
-          <NavItem eventKey={3} href='#'>About</NavItem>
-          <NavItem eventKey={4} href='#'>Contact</NavItem>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+class HomeNavigation extends Component {
+  render() {
+    return (
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav pullRight>
+            <LinkContainer to='/music'>
+              <NavItem>Music</NavItem>
+            </LinkContainer>
+            <LinkContainer to='/code'>
+              <NavItem>Code</NavItem>
+            </LinkContainer>
+            <LinkContainer to='/blog'>
+              <NavItem>Blog</NavItem>
+            </LinkContainer>
+            <LinkContainer to='/about'>
+              <NavItem>About</NavItem>
+            </LinkContainer>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
 }
 
 function Footer() {
   return(
-    <Grid className="footer">
+    <Grid className='footer'>
       <Row>
         <Col xs={4}>
-          <a href="https://www.youtube.com/channel/UCwzBKt0x2hl66aaL2QuqF0w"
-            target="_blank"
-            rel="noopener noreferrer">
-            <Youtube className="social-icon"/>
+          <a href='https://www.youtube.com/channel/UCwzBKt0x2hl66aaL2QuqF0w'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <Youtube className='social-icon'/>
           </a>
         </Col>
         <Col xs={4}>
-          <a href="https://github.com/pengmai"
-            target="_blank"
-            rel="noopener noreferrer">
-            <Github className="social-icon"/>
+          <a href='https://github.com/pengmai'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <Github className='social-icon'/>
           </a>
         </Col>
         <Col xs={4}>
-          <a href="https://www.facebook.com/jacob.peng.5"
-            target="_blank"
-            rel="noopener noreferrer">
-            <Facebook className="social-icon"/>
+          <a href='https://www.facebook.com/jacob.peng.5'
+            target='_blank'
+            rel='noopener noreferrer'>
+            <Facebook className='social-icon'/>
           </a>
         </Col>
       </Row>
     </Grid>
   );
 }
-class App extends Component {
+
+function Code() {
+  return (
+    <h1>Code</h1>
+  );
+}
+
+function Music() {
+  return (
+    <h1>Music</h1>
+  );
+}
+
+function Blog() {
+  return (
+    <h1>Blog</h1>
+  );
+}
+
+function About() {
+  return (
+    <h1>About/Contact</h1>
+  );
+}
+
+class Home extends Component {
   render() {
     return (
       <div>
@@ -70,6 +113,22 @@ class App extends Component {
         <HomeNavigation/>
         <Footer/>
       </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={Home}/>
+          <Route path='/music' component={Music}/>
+          <Route path='/code' component={Code}/>
+          <Route path='/blog' component={Blog}/>
+          <Route path='/about' component={About}/>
+        </div>
+      </Router>
     );
   }
 }
