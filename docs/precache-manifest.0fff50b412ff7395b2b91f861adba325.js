21a721ee7f57e35eaa25eba07f810398ea994e32
Jacob Peng <jacob.peng@hotmail.com>
2018-11-19 02:26:13 -0500

    Adding static bundles

---
+++ docs/precache-manifest.0fff50b412ff7395b2b91f861adba325.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f2dc1136e79a267fa3fe4d4175b5b27",
+    "url": "/static/media/workouttracker.8f2dc113.png"
+  },
+  {
+    "revision": "ca332c861b84472b050c66c50f55295a",
+    "url": "/static/media/prelude.ca332c86.jpg"
+  },
+  {
+    "revision": "16975ca424cc84f92c0931a67ef4942a",
+    "url": "/static/media/me.16975ca4.jpg"
+  },
+  {
+    "revision": "577f870821422b03c00bfefae3b4bfaa",
+    "url": "/static/media/incandescent.577f8708.jpg"
+  },
+  {
+    "revision": "0d029e30b86ea90a7ec64d4ba09de8e3",
+    "url": "/static/media/aStoryAboutLove.0d029e30.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6780aee8f65239a81ed9",
+    "url": "/static/js/main.6780aee8.chunk.js"
+  },
+  {
+    "revision": "89558aee16474561bc3c",
+    "url": "/static/js/1.89558aee.chunk.js"
+  },
+  {
+    "revision": "6780aee8f65239a81ed9",
+    "url": "/static/css/main.208b9b80.chunk.css"
+  },
+  {
+    "revision": "6e9242f38a2fc757f7e33d19a61e6e8b",
+    "url": "/index.html"
+  }
+];
