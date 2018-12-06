15dc5fe35b88712f70024d7a3be971543f8c18a9
Jacob Peng <jacob.peng@hotmail.com>
2018-12-06 17:07:34 -0500

    Adding feelings to music

--- src/views/Music.tsx
+++ src/views/Music.tsx
@@ -5,8 +5,32 @@ import { Footer } from './Footer';
 import { Navigation } from './Navigation';
 
 import aStoryImg from '../images/aStoryAboutLove.jpg';
+import feelingsImg from '../images/feelings.jpg';
 import incandescentImg from '../images/incandescent.jpg';
 import preludeImg from '../images/prelude.jpg';
+
+const images = [
+  {
+    href: 'https://soundcloud.com/acasurroundsound/feelings',
+    src: feelingsImg,
+    caption: 'Feelings'
+  },
+  {
+    href: 'https://indigovioletca.bandcamp.com/album/prelude-the-demo',
+    src: preludeImg,
+    caption: 'Prelude'
+  },
+  {
+    href: 'https://jacobmai.bandcamp.com/releases',
+    src: aStoryImg,
+    caption: 'A Story About Love'
+  },
+  {
+    href: 'https://thesyntheticarmy.bandcamp.com/album/incandescent',
+    src: incandescentImg,
+    caption: 'Incandescent'
+  }
+];
 
 export function Music() {
   return (
@@ -22,27 +46,15 @@ export function Music() {
           </Col>
         </Row>
         <Row>
-          <Col xs={12} sm={6} md={4}>
-            <ExternalPreview
-              href="https://indigovioletca.bandcamp.com/album/prelude-the-demo"
-              src={preludeImg}
-              caption="Prelude"
-            />
-          </Col>
-          <Col xs={12} sm={6} md={4}>
-            <ExternalPreview
-              href="https://jacobmai.bandcamp.com/releases"
-              src={aStoryImg}
-              caption="A Story About Love"
-            />
-          </Col>
-          <Col xs={12} sm={6} md={4}>
-            <ExternalPreview
-              href="https://thesyntheticarmy.bandcamp.com/album/incandescent"
-              src={incandescentImg}
-              caption="Incandescent"
-            />
-          </Col>
+          {images.map(im =>
+            <Col xs={12} sm={6}>
+              <ExternalPreview
+                href={im.href}
+                src={im.src}
+                caption={im.caption}
+                />
+            </Col>
+          )}
         </Row>
       </Grid>
       <Footer/>
