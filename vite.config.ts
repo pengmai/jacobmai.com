698d396ac4ad9a76de91617cbbb844be9d681d98
Jacob Peng <jacobmpeng@gmail.com>
2023-02-20 12:06:24 -0500

    Add Yarn v2, update sudoku solver link, add basic code page

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,37 @@ import { defineConfig } from 'vite';
 import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import { resolve } from 'path';
 
+const albums = [
+  {
+    title: 'Feelings',
+    href: 'https://soundcloud.com/acasurroundsound/feelings',
+    img: '/images/feelings.jpg',
+    alt: 'Feelings - Single',
+    year: '2018',
+  },
+  {
+    title: 'Prelude',
+    href: 'https://indigovioletca.bandcamp.com/album/prelude-the-demo',
+    img: '/images/prelude.jpg',
+    alt: 'Prelude the Demo',
+    year: '2016',
+  },
+  {
+    title: 'A Story About Love',
+    href: 'https://jacobmai.bandcamp.com/releases',
+    img: '/images/aStoryAboutLove.jpg',
+    alt: 'A Story About Love',
+    year: '2015',
+  },
+  {
+    title: 'Incandescent',
+    href: 'https://thesyntheticarmy.bandcamp.com/album/incandescent',
+    img: '/images/incandescent.jpg',
+    alt: 'Incandescent',
+    year: '2014',
+  },
+];
+
 export default defineConfig({
   build: {
     rollupOptions: {
@@ -9,16 +40,17 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         music: resolve(__dirname, 'music/index.html'),
         code: resolve(__dirname, 'code/index.html'),
+        sudokusolver: resolve(__dirname, 'code/sudokusolver.html'),
       },
     },
   },
   plugins: [
-    ViteEjsPlugin(),
+    ViteEjsPlugin({ albums }),
     {
       name: 'template-reload',
       enforce: 'post',
       handleHotUpdate({ file, server }) {
-        if (file.endsWith('.ejs')) {
+        if (file.endsWith('.ejs') || file.endsWith('.html')) {
           server.ws.send({ type: 'full-reload', path: '*' });
         }
       },
